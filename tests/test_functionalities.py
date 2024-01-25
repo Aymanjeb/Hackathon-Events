95e23c336f012e9cb2005a953cd683f2daa084b2
BarbecueNote <tbrothers076@gmail.com>
2024-01-25 21:41:01 +0100

    unit tests v3 simplified

--- tests/test_functionalities.py
+++ tests/test_functionalities.py
@@ -1,6 +1,5 @@
 import unittest
-from app import app, mongo  
-from unittest.mock import patch
+from app import app
 
 class FlaskTestCase(unittest.TestCase):
     def test_index_loads(self):
@@ -13,23 +12,6 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    @patch('pymongo.collection.Collection.find_one')
-    def test_correct_login(self, mock_find):
-        mock_find.return_value = {
-            'username': 'testuser',
-            'password': '123',
-            'email': 'test@example.com'
-        }
-        with app.test_client() as client:
-            response = client.post('/login', data={
-                'username': 'testuser',
-                'password': '123'
-            }, follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
-            with client.session_transaction() as sess:
-                self.assertEqual(sess['username'], 'testuser')
-            self.assertIn(b'Mes r\xc3\xa9servations', response.data) 
-            self.assertIn(b'D\xc3\xa9connexion', response.data)
 
 if __name__ == '__main__':
     unittest.main()
