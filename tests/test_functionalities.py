ea4f946fd7edf72d0da89a6108c96fa23e6e4f37
BarbecueNote <tbrothers076@gmail.com>
2024-01-25 21:29:56 +0100

    unit tests v1 w/ finished front

--- tests/test_functionalities.py
+++ tests/test_functionalities.py
@@ -1,39 +1,35 @@
-import os
 import unittest
-from unittest.mock import patch, MagicMock
-from app import app, fetch_movies
+from app import app, mongo  
+from unittest.mock import patch
 
 class FlaskTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True 
-
-    def test_home_status_code(self):
-        response = self.app.get('/')
+    def test_index_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_search_status_code(self):
-        response = self.app.post('/search', data=dict(movieQuery="killers of the flower moon"))
+    def test_login_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        
-    @patch('app.requests.get')
-    def test_fetch_movies(self, mock_get):
-        api_key = os.getenv('API_KEY')
-        mock_movies_response = {"results":
-            [{'adult': False, 'backdrop_path': '/1X7vow16X7CnCoexXh4H4F2yDJv.jpg', 'genre_ids': [80, 18, 36], 'id': 466420, 'original_language': 'en', 'original_title': 'Killers of the Flower Moon', 'overview': 'When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered one by one—until the FBI steps in to unravel the mystery.', 'popularity': 628.26, 'poster_path': '/dB6Krk806zeqd0YNp2ngQ9zXteH.jpg', 'release_date': '2023-10-18', 'title': 'Killers of the Flower Moon', 'video': False, 'vote_average': 7.525, 'vote_count': 1892}],
-              "total_pages":1,
-              "total_results":1
-              }
-        mock_response = MagicMock()
-        mock_response.json.return_value = mock_movies_response
-        mock_get.return_value = mock_response
 
-        movies = fetch_movies("killers%20of%20the%20flower%20moon")
-        mock_get.assert_called_once_with(
-            f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=killers%20of%20the%20flower%20moon"
-        )
-        self.assertEqual(movies, mock_movies_response['results'])
+    @patch('pymongo.collection.Collection.find_one')
+    def test_correct_login(self, mock_find):
+        mock_find.return_value = {
+            'username': 'testuser',
+            'password': '123',
+            'email': 'test@example.com'
+        }
+        with app.test_client() as client:
+            response = client.post('/login', data={
+                'username': 'testuser',
+                'password': '123'
+            }, follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            with client.session_transaction() as sess:
+                self.assertEqual(sess['username'], 'testuser')
+            self.assertIn(b'Mes r\xc3\xa9servations', response.data) 
+            self.assertIn(b'D\xc3\xa9connexion', response.data)
 
 if __name__ == '__main__':
     unittest.main()
